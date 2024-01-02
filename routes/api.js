016b69f0c789be09c17e494f1c299eafe434a267
salmanlfarisi <alfarissalman2419@gmail.com>
2024-01-02 21:16:52 +0700

    Tugas Praktikum13

--- routes/api.js
+++ routes/api.js
@@ -17,6 +17,7 @@ router.get("/students", StudentController.index);
 router.post("/students", StudentController.store);
 router.put("/students/:id", StudentController.update);
 router.delete("/students/:id", StudentController.destroy);
+router.get("/students/:id", StudentController.show);
 
 // export routing
 module.exports = router;
